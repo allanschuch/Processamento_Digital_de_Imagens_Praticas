b2b439c761c55d57fa06ac164628dfe54f42b7fb
Allan <allan20.1@hotmail.com>
2024-08-08 17:57:45 -0300

    function contrast working

--- contraste.m
+++ contraste.m
@@ -1,13 +1,36 @@
-function newImage = constraste(image)
+function newImageName = contraste(image, L)
 
-pkg load image;
-originalHist = imhist(image)
+    pkg load image;
 
+    figure;
+    subplot(2,2,1);
+    imshow(image);
+    
+    originalHist = imhist(image);
+
+    subplot(2,2,2);
+    bar(originalHist);
+
+    normalizedHist = normalizeHist(originalHist,image);
+    accHist = cumsum(normalizedHist);
+    lookUpTable = getLookUptable(accHist, L);
+    newImage = arrayfun(@(x) lookUpTable(x), image);
+
+    subplot(2,2,3);
+    imshow(newImage);
+
+    subplot(2,2,4);
+    imhist(newImage);
+
+    newImageName = 'result_best_constrast.bmp';
+    imwrite(newImage,newImageName);
 end
 
-function accHist = getAccHist(histTable)
-    accHist{1} = [histTable(1), histTable(1)];
-    for i = 2:numel(histTable)
-        accHist{i} = [histTable(i), histTable(i) + accHist{i-1}];
-    end
+function normalizedHist = normalizeHist(originalHist, image)
+    [lines, cols] = size(image);
+    normalizedHist = originalHist / (lines * cols);
+end
+
+function lookUpTable = getLookUptable(accHist, L)
+    lookUpTable = uint8(accHist(:) * (L-1));
 end
