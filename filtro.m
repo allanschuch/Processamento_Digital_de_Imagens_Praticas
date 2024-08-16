357c800d4c5606d888b3179c08af4a8c2ee00e87
Allan <allan20.1@hotmail.com>
2024-08-16 18:29:46 -0300

    Init implementation of extendMatrix

--- filtro.m
+++ filtro.m
@@ -1,8 +1,19 @@
-function filteredImage = filtro(image, weight)
-    filteredImage = invertFilter(image);
+function filteredImage = filtro(image,filter,weight)
+    filter = invertFilter(filter);
+    filteredImage = extendMatrix(image, filter)
 end
 
 function invertedFilter = invertFilter(filter)
     invertedFilter = fliplr(filter);
     invertedFilter = flipud(invertedFilter);
+end
+
+function extendedMatrix = extendMatrix(matrix,filter)
+    filterSize = size(filter, 2);
+    offset = floor(filterSize/2)
+    [matrixRows, matrixCols] = size(matrix);
+    extendedMatrix = zeros(matrixRows + (2 * offset), matrixCols + (2 * offset));
+    [extendedMatrixRows, extendedMatrixCols] = size(extendedMatrix);
+
+    extendedMatrix(offset + 1:extendedMatrixRows - offset, offset + 1:extendedMatrixCols - offset) = matrix(:,:);
 end
