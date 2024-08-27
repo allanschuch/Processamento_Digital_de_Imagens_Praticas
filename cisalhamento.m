c4872fac1086940b2aa8be9810be188765cc7759
Allan <allan20.1@hotmail.com>
2024-08-26 23:06:25 -0300

    exercise done

--- cisalhamento.m
+++ cisalhamento.m
@@ -1,6 +1,7 @@
-function resultImage = cisalhamento(image, cv, ch)
+function resultImageName = cisalhamento(image_name, cv, ch)
+    image = imread(image_name);
     [imageRows imageCols] = size(image);
-    resultImage = zeros(imageRows + (imageCols*cv), imageCols + (imageRows*ch));
+    resultImage = zeros(imageRows + ceil(imageCols*cv), imageCols + ceil(imageRows*ch));
 
     T = [1,ch,0;
          cv,1,0;
@@ -11,7 +12,19 @@ function resultImage = cisalhamento(image, cv, ch)
     for i = 1:size(resultImage, 1)
         for j = 1:size(resultImage, 2)
             coord = afim(i,j,Tinv);
-            resultImage(i,j) = image(coord(1), coord(2)); 
+            row = ceil(coord(1));
+            col = ceil(coord(2));
+            if (row <= imageRows && col <= imageCols)
+                if (row > 0 && col > 0)
+                    resultImage(i,j) = image(row, col);
+                end
+            end  
         end
     end
+
+    resultImage = uint8(rescale(resultImage,0,255));
+
+    resultImageName = 'imagem_cisalhamento.bmp';
+
+    imwrite(resultImage,resultImageName);
 end
