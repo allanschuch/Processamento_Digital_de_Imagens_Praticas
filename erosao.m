4ad5dc16d391b0bca68d4e78b26bc7361a2fd636
Allan <allan20.1@hotmail.com>
2024-10-11 16:59:05 -0300

    exercise ready

--- erosao.m
+++ erosao.m
@@ -1,8 +1,18 @@
-function imagem_erodida = erosao(imagem, elem_est)
-    imagem_erodida = extendMatrix(imagem, elem_est);
-    %matrix_extendida = extendMatrix(imagem, elem_est);
-    %imagem_erodida = zeros(size(imagem,1), size(imagem,2));
-    %offsetSize = floor(size(elem_est,1)/2);
+function imagem_erodida_name = erosao(imagem, elem_est)
+    matrix_extendida = extendMatrix(imagem, elem_est);
+    imagem_erodida = zeros(size(imagem,1), size(imagem,2));
+    elem_est_size = size(elem_est);
+    offset_rows = floor(elem_est_size(1)/2);
+    offset_cols = floor(elem_est_size(2)/2);
+
+    for i = 1:size(imagem_erodida,1)
+        for j = 1:size(imagem_erodida,2)
+            imagem_erodida(i,j) = aplicaErosao(matrix_extendida, elem_est, i+offset_rows, j+offset_cols, offset_cols,offset_rows);
+        end
+    end
+
+    imagem_erodida_name = 'imagem_erodida.bmp';
+    imwrite(imagem_erodida, imagem_erodida_name);
 end
 
 function extendedMatrix = extendMatrix(matrix,filter)
@@ -15,8 +25,12 @@ function extendedMatrix = extendMatrix(matrix,filter)
     extendedMatrix(offset_rows + 1:extendedMatrixRows - offset_rows, offset_cols + 1:extendedMatrixCols - offset_cols) = matrix(:,:);
 end
 
-function newValue = aplicaErosao(matrix, elem_est, row, col, offsetSize)
-    soma_elementos_do_elem_est = sum(sum(elem_est));
-    tempMatrix = matrix(row-offsetSize:row+offsetSize, col-offsetSize:col+offsetSize) .* elem_est;
-    newValue = soma_elementos_do_elem_est == tempMatrix;
+function newValue = aplicaErosao(matrix, elem_est, row, col, offset_cols, offset_rows)
+    soma_elementos_do_elem_est = sum(sum(elem_est)) + 1;
+    tempMatrix = matrix(row-offset_rows:row+offset_rows, col-offset_cols:col+offset_cols) .* elem_est;
+    if (soma_elementos_do_elem_est == sum(sum(tempMatrix)) + 1)
+        newValue = 1;
+    else 
+        newValue = 0;
+    end
 end
